f1d351d05bf0906d1e0d139713c4480135a4e5c4
wliu080 <wliu080@aucklanduni.ac.nz>
2022-08-29 12:53:32 +0900

    Fixed layouts for the markdown generated pages

--- markdownContent/toolkit.md
+++ markdownContent/toolkit.md
@@ -3,4 +3,4 @@ title: 'Toolkit'
 subtitle: 'Coming Soon'
 ---
 
-Downloads and other resources
+Downloads and other resources Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et neque vulputate, efficitur elit nec, dignissim turpis. Nullam tincidunt sem non felis aliquet gravida. Fusce nunc enim, tristique et risus non, volutpat convallis leo. Praesent tincidunt fermentum mi, a viverra nibh luctus faucibus. Proin hendrerit massa vel quam condimentum, at aliquam justo porta. Aenean quis tortor augue. Vivamus id mi placerat, bibendum ligula a, ultricies ligula. Donec quis lobortis lorem, at vestibulum nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
--- pages/[markdownPage].tsx
+++ pages/[markdownPage].tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { getAllStaticPageIds, getStaticContent } from "../lib/markdownPageServer";
 import { GetStaticProps, GetStaticPaths } from "next";
+import { Col, Container, Row, Stack } from "react-bootstrap";
+import Navigation from "../components/navigation";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllStaticPageIds();
@@ -38,10 +40,40 @@ export default function MarkdownPage({
       <Head>
         <title>{markdownContent.title}</title>
       </Head>
-      <article>
-        <h1>{markdownContent.subtitle}</h1>
-        <div dangerouslySetInnerHTML={{ __html: markdownContent.contentHtml }} />
-      </article>
+      <Container
+        className="cover-hero md-banner"
+        fluid
+        style={{
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <Navigation />
+        <Stack gap={1} direction="horizontal" className="mx-auto cover">
+          <Container className="text-primary text-center text-uppercase">
+            <h1 className="display-6">{markdownContent.title}</h1>
+          </Container>
+        </Stack>
+      </Container>
+      <Container fluid className="bg-light place-items-center">
+        <article>
+          <h1 className="text-center place-content-center pt-5">{markdownContent.subtitle}</h1>
+          <Container>
+            <div className="align-self-center" dangerouslySetInnerHTML={{ __html: markdownContent.contentHtml }} />
+          </Container>
+        </article>
+        {/* <Stack gap={3} direction="horizontal" className="mx-auto pt-5">
+          <article>
+            <h1 className="text-center">{markdownContent.subtitle}</h1>
+            <Row>
+              <Col md="3"></Col>
+              <Col className="align-self-center text-dark">
+                <div dangerouslySetInnerHTML={{ __html: markdownContent.contentHtml }} />
+              </Col>
+              <Col md="3"></Col>
+            </Row>
+          </article>
+        </Stack> */}
+      </Container>
     </div>
   );
 }
